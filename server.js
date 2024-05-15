39f22325deee23fac6eb847f2af374d9b16fc18b
setongee <setongee@gmail.com>
2024-05-15 13:32:10 +0100

    added dotenv file config

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const { Mdas_Dataset } = require("./models/mda.model.js");
 const MdaRoutes = require("./routes/Mda.routes.js");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded( { extended : false } ) );
@@ -13,7 +14,7 @@ const base_url = '/api/v2'
 app.use(`${base_url}/mdas`, MdaRoutes );
 
 
-mongoose.connect('mongodb+srv://lasgadmindatabase:NyU4M00EzxfMRaFs@lasgv2db.z4d3eki.mongodb.net/LASG_API?retryWrites=true&w=majority&appName=lasgv2DB')
+mongoose.connect(`mongodb+srv://lasgadmindatabase:${process.env.DB_PASSWORD}@lasgv2db.z4d3eki.mongodb.net/LASG_API?retryWrites=true&w=majority&appName=lasgv2DB`)
     .then(() => {
         console.log('Database is Connected!');
         app.listen(8000, () => {
