84c0dd44d95930289bb1736b19f29073e70f57c8
setongee <setongee@gmail.com>
2024-11-18 21:12:29 +0100

    yayyy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const port = 8000
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit : '50mb'}));
 app.use(express.urlencoded({extended : false, limit: '50mb'}));
 
 const MdaRoutes = require("./routes/Mda.routes.js");
